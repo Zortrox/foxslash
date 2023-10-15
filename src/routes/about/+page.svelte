db7ee949ab252b1e6e5f2a3817249477dedbd2f3
Zortrox <zortroxofdoom@gmail.com>
2023-10-15 16:19:52 -0500

    feat: About page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,20 @@
+<h1 class="text-2xl mb-5">About Me</h1>
+<p class="mb-3">
+	Hey there, I'm Zortrox, just a guy with a bunch of cool interests and roles in life. I spend my
+	days working as a programmer, where I love to mess around with programming and electronics
+	projects, especially Raspberry Pis and other self-hosted software. It's all about having fun and
+	learning as I go.
+</p>
+<p class="mb-3">
+	When I'm off the clock, you'll often find me immersed in Warframe, League of Legends, or Guild
+	Wars 2 (or whatever other game I'm hyper-fixating on). I'm also a dedicated family man, cherishing
+	the quality time I spend with my wife and daughter, whether it's gaming (video or board games),
+	spending time outdoors, or visiting imaginary worlds.
+</p>
+<p class="mb-3">
+	Beyond that, I'm a fan of science fiction and fantasy books and find peace in the great outdoors
+	through activities like hiking and swimming. I also love Dungeons & Dragons, where I get to let my
+	creativity run wild (even though I don't play as often as I want). So, yeah, I'm just a regular
+	guy with a mix of interests and roles, and it's all about having a good time and sharing the joy
+	with others.
+</p>
